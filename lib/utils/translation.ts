fc8d130c9af51a05ad61759cf1726f6baa2373a0
Zoltan Maroti <zoli.maroti@gmail.com>
2023-07-02 21:48:21 +0200

    relative import locales

--- lib/utils/translation.ts
+++ lib/utils/translation.ts
@@ -3,7 +3,7 @@ import { Locale, Translation } from '@/lib/types/types';
 
 export async function getTranslation(locale: Locale): Promise<Translation> {
     try {
-        return (await import(`../../locales/${locale}.json`)).default;
+        return (await import(`@/locales/${locale}.json`)).default;
     } catch (error) {
         notFound();
     }
