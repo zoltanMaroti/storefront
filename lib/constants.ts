476303a61de98a20a95f1b8c90d907ebd5dbe039
Zoltan Maroti <zoli.maroti@gmail.com>
2023-07-05 16:08:07 +0200

    feat(product): created graphql api client

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,8 @@
 import { Category, LanguageSelectorItem } from '@/lib/types';
 
+export const API_URL = process.env.NEXT_PUBLIC_COMMERCE_PROVIDER_API_URL;
+export const API_TOKEN = process.env.NEXT_PUBLIC_COMMERCE_PROVIDER_ACCESS_TOKEN;
+
 export const screenSizes = {
     SMALL: '768px',
     MEDIUM: '1024px',
