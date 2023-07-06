a7601bf3787b8ffbdc9837b099cfdc582d494a01
Zoltan Maroti <zoli.maroti@gmail.com>
2023-07-06 12:03:31 +0200

    feat(product): allowed image hostname in next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 const withNextIntl = require('next-intl/plugin')('./i18n.ts');
 
-module.exports = withNextIntl({});
+module.exports = withNextIntl({
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'a.storyblok.com',
+            },
+        ],
+    },
+});
