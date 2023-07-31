4b6a2ef033c9b0472d331065709c8755fa676ef3
Zoltan Maroti <zoli.maroti@gmail.com>
2023-07-31 13:51:24 +0200

    fix(admin): updated admin root layout prop type

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,5 @@
 import { AdminProviders } from '@/lib/providers/admin';
-import { GenericContainerProps } from '@/lib/types';
+import { LayoutProps } from '@/lib/types';
 import '@/app/[locale]/globals.css';
 import { Montserrat } from 'next/font/google';
 
@@ -7,7 +7,7 @@ const montserrat = Montserrat({
     subsets: ['latin'],
     weight: ['300', '400', '500', '600'],
 });
-export default function RootLayout({ children }: GenericContainerProps) {
+export default function RootLayout({ children }: LayoutProps) {
     return (
         <html lang="en">
             <body className={montserrat.className}>
