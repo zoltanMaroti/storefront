c8e5600d13592edb7683a389d36bc460344c8ca9
Zoltan Maroti <zoli.maroti@gmail.com>
2023-07-31 15:24:40 +0200

    feat(admin): secure get checkout session by payment intent id endpoint

--- app/api/payment/[paymentIntentId]/route.ts
+++ app/api/payment/[paymentIntentId]/route.ts
@@ -1,11 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getAllSessions } from '@/lib/services/checkout';
 import { PaymentIntentRequestParams } from '@/lib/types';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 export async function GET(
     request: NextRequest,
     { params }: { params: PaymentIntentRequestParams }
 ) {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
     const { paymentIntentId } = params;
 
     if (!paymentIntentId) {
@@ -18,6 +26,7 @@ export async function GET(
     try {
         const checkoutSession = await getAllSessions({
             payment_intent: paymentIntentId,
+            expand: ['data.line_items'],
         });
 
         return NextResponse.json(checkoutSession);
