2a70589835a71c9b2db3bcf2d3d5379d6121184d
Zoltan Maroti <zoli.maroti@gmail.com>
2023-08-01 16:45:38 +0200

    feat(admin): refetch payment intents when tracking number mutation is settled

--- lib/api/payment.ts
+++ lib/api/payment.ts
@@ -96,6 +96,6 @@ export const PaymentApiClient: IPaymentApiClient = {
 
         const data = await response.json();
 
-        return data;
+        return data.paymentIntent;
     },
 };
--- lib/hooks/useTrackingNumber.tsx
+++ lib/hooks/useTrackingNumber.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { PaymentApiClient } from '@/lib/api/payment';
 import { CreateTrackingNumberParams } from '@/lib/types';
 
 const useTrackingNumber = () => {
+    const queryClient = useQueryClient();
+
     return useMutation({
         mutationFn: ({
             paymentIntentId,
@@ -13,6 +15,11 @@ const useTrackingNumber = () => {
                 paymentIntentId,
                 trackingNumber
             ),
+        onSettled: (paymentIntent) => {
+            if (paymentIntent) {
+                queryClient.refetchQueries(['paymentIntents']);
+            }
+        },
     });
 };
 
