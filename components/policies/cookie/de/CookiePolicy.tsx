a847aa82bc8a01710fb670367e1b104512bb4a0e
Zoltan Maroti <zoli.maroti@gmail.com>
2023-08-05 15:38:10 +0200

    feat(CookiePolicy): German and French cookie policies

--- components/policies/cookie/de/CookiePolicy.tsx
+++ components/policies/cookie/de/CookiePolicy.tsx
@@ -1,7 +1,261 @@
 import React from 'react';
+import { Grid, List } from '@/components/policies/style';
 
 const CookiePolicy = () => {
-    return <div>TODO</div>;
+    return (
+        <div>
+            <h1>Cookie-Richtlinie</h1>
+            <p>Letztes Update: 05/08/2023</p>
+            <ol>
+                <li>
+                    <span>Einführung</span>
+                    <p>
+                        In dieser Cookie-Richtlinie wird erläutert, wie [Ihr
+                        E-Commerce-Shop] (&quot;wir&quot;, &quot;uns&quot; oder
+                        &quot;unser&quot;) Cookies und ähnliche Technologien
+                        verwendet, um Sie zu erkennen, wenn Sie unsere Website
+                        unter [Domain] (&quot;Website&quot;) besuchen. Es
+                        erklärt, was diese Technologien sind und warum wir sie
+                        nutzen, sowie Ihre Rechte, unsere Nutzung dieser
+                        Technologien zu kontrollieren.
+                    </p>
+                </li>
+                <li>
+                    <span>Was sind Cookies?</span>
+                    <p>
+                        Cookies sind kleine Datendateien, die auf Ihrem Gerät
+                        (Computer, Tablet, Smartphone usw.) abgelegt werden,
+                        wenn Sie eine Website besuchen. Diese Cookies helfen der
+                        Website, sich im Laufe der Zeit an Ihre Aktionen und
+                        Präferenzen zu erinnern, was Ihr Surferlebnis
+                        verbessert. Cookies werden von Website-Eigentümern
+                        häufig verwendet, damit ihre Websites funktionieren oder
+                        effizienter funktionieren und um Berichtsinformationen
+                        bereitzustellen.
+                    </p>
+                </li>
+                <li>
+                    <span>Wie verwenden wir Cookies?</span>
+                    <p>
+                        Wir verwenden aus mehreren Gründen Erst- und
+                        Drittanbieter-Cookies. Diese Cookies sind aus
+                        technischen Gründen für den Betrieb unserer Website
+                        erforderlich und wir bezeichnen sie als
+                        &quot;wesentliche&quot; oder &quot;unbedingt
+                        notwendige&quot; Cookies. Dies wird im Folgenden näher
+                        beschrieben.
+                    </p>
+                </li>
+                <li>
+                    <span>Welche Arten von Cookies verwenden wir?</span>
+                    <ol>
+                        <li>
+                            Notwendig
+                            <p>
+                                Notwendige Cookies sind für die ordnungsgemäße
+                                Funktion der Website unbedingt erforderlich.
+                                Diese Cookies stellen anonym grundlegende
+                                Funktionalitäten und Sicherheitsmerkmale der
+                                Website sicher.
+                            </p>
+                            <p>
+                                In der folgenden Liste sind die auf unserer
+                                Website verwendeten Cookies aufgeführt.
+                            </p>
+                            <List>
+                                <Grid>
+                                    <p>Name:</p>
+                                    <p>__stripe_mid</p>
+                                    <p>Domain:</p>
+                                    <p>.checkout.stripe.com, request</p>
+                                    <p>Dauer:</p>
+                                    <p>1 Jahr</p>
+                                    <p>Zweck:</p>
+                                    <p>
+                                        Wird zur Betrugsprävention festgelegt
+                                        und hilft uns, das mit einer versuchten
+                                        Transaktion verbundene Risiko
+                                        einzuschätzen.
+                                    </p>
+                                </Grid>
+                                <Grid>
+                                    <p>Name:</p>
+                                    <p>__stripe_sid</p>
+                                    <p>Domain:</p>
+                                    <p>.checkout.stripe.com, request</p>
+                                    <p>Dauer:</p>
+                                    <p>30 Minuten</p>
+                                    <p>Zweck:</p>
+                                    <p>
+                                        Wird zur Betrugsprävention festgelegt
+                                        und hilft uns, das mit einer versuchten
+                                        Transaktion verbundene Risiko
+                                        einzuschätzen.
+                                    </p>
+                                </Grid>
+                                <Grid>
+                                    <p>Name:</p>
+                                    <p>m</p>
+                                    <p>Domain:</p>
+                                    <p>.m.stripe.com, request</p>
+                                    <p>Dauer:</p>
+                                    <p>2 Jahre</p>
+                                    <p>Zweck:</p>
+                                    <p>
+                                        Zur Betrugserkennung. Hilft Stripe bei
+                                        der Einschätzung des Risikos, das mit
+                                        einer versuchten Transaktion auf Ihrer
+                                        Website verbunden ist.
+                                    </p>
+                                </Grid>
+                                <Grid>
+                                    <p>Name:</p>
+                                    <p>_GRECAPTCHA</p>
+                                    <p>Domain:</p>
+                                    <p>.google.com, request</p>
+                                    <p>Dauer:</p>
+                                    <p>6 Monate</p>
+                                    <p>Zweck:</p>
+                                    <p>
+                                        Cookie, das von der
+                                        ReCaptcha-Funktionalität verwendet wird,
+                                        um eine Anti-Bot-Validierung in
+                                        verschiedenen Formen zu ermöglichen.
+                                    </p>
+                                </Grid>
+                                <Grid>
+                                    <p>Name:</p>
+                                    <p>NEXT_LOCALE</p>
+                                    <p>Domain:</p>
+                                    <p>[Domain]</p>
+                                    <p>Dauer:</p>
+                                    <p>Session</p>
+                                    <p>Zweck:</p>
+                                    <p>
+                                        Dieses Cookie wird verwendet, um die
+                                        bevorzugte Sprache des Besuchers zu
+                                        ermitteln und die Sprache auf der
+                                        Website nach Möglichkeit entsprechend
+                                        einzustellen.
+                                    </p>
+                                </Grid>
+                            </List>
+                        </li>
+                    </ol>
+                </li>
+                <li>
+                    <span>Wie kann ich Cookies in meinem Browser steuern?</span>
+                    <p>
+                        Sie haben das Recht, Ihre Cookie-Einstellungen zu
+                        verwalten. Die meisten Webbrowser akzeptieren Cookies
+                        automatisch, aber Sie können Ihre Browsereinstellungen
+                        normalerweise so ändern, dass Cookies abgelehnt werden,
+                        wenn Sie dies bevorzugen. Bitte beachten Sie, dass das
+                        Blockieren oder Löschen von Cookies Ihr Surferlebnis
+                        beeinträchtigen und den Zugriff auf bestimmte Funktionen
+                        unserer Website einschränken kann. Um mehr über die
+                        Verwaltung von Cookies zu erfahren, können Sie die
+                        Hilfeseiten Ihres Browsers oder Geräts besuchen.
+                    </p>
+                    <ol>
+                        <li>
+                            Chrome{' '}
+                            <p>
+                                <a
+                                    href="https://support.google.com/chrome/answer/95647#zippy=%2Callow-or-block-cookies"
+                                    target={'_blank'}
+                                >
+                                    Clear, allow & manage cookies in Chrome
+                                </a>
+                            </p>
+                        </li>
+                        <li>
+                            Internet Explorer{' '}
+                            <p>
+                                <a
+                                    href="https://support.microsoft.com/en-us/windows/delete-and-manage-cookies-168dab11-0753-043d-7c16-ede5947fc64d"
+                                    target={'_blank'}
+                                >
+                                    Delete and manage cookies
+                                </a>
+                            </p>
+                        </li>
+                        <li>
+                            Firefox{' '}
+                            <p>
+                                <a
+                                    href="https://support.mozilla.org/en-US/kb/enhanced-tracking-protection-firefox-desktop?redirectslug=enable-and-disable-cookies-website-preferences&redirectlocale=en-US"
+                                    target={'_blank'}
+                                >
+                                    Enhanced Tracking Protection in Firefox for
+                                    desktop
+                                </a>
+                            </p>
+                        </li>
+                        <li>
+                            Safari{' '}
+                            <p>
+                                <a
+                                    href="https://support.apple.com/en-ie/guide/safari/sfri11471/mac"
+                                    target={'_blank'}
+                                >
+                                    Clear cookies in Safari on Mac
+                                </a>
+                            </p>
+                        </li>
+                        <li>
+                            Edge{' '}
+                            <p>
+                                <a
+                                    href="https://support.microsoft.com/en-us/windows/microsoft-edge-browsing-data-and-privacy-bb8174ba-9d73-dcf2-9b4a-c582b4e640dd"
+                                    target={'_blank'}
+                                >
+                                    Microsoft Edge, browsing data, and privacy
+                                </a>
+                            </p>
+                        </li>
+                        <li>
+                            Opera{' '}
+                            <p>
+                                <a
+                                    href="https://help.opera.com/en/latest/web-preferences/"
+                                    target={'_blank'}
+                                >
+                                    Web preferences
+                                </a>
+                            </p>
+                        </li>
+                    </ol>
+                </li>
+                <li>
+                    <span>
+                        Änderungen an dieser Cookie-Richtlinie
+                        <p>
+                            Wir können diese Cookie-Richtlinie von Zeit zu Zeit
+                            aktualisieren, um Änderungen in der Technologie oder
+                            rechtlichen Anforderungen Rechnung zu tragen. Bitte
+                            lesen Sie diese Cookie-Richtlinie daher regelmäßig
+                            durch, um über unsere Verwendung von Cookies und
+                            verwandten Technologien informiert zu bleiben. Das
+                            Datum oben in dieser Cookie-Richtlinie gibt an, wann
+                            sie zuletzt aktualisiert wurde.
+                        </p>
+                    </span>
+                </li>
+                <li>
+                    <span>
+                        Kontaktiere uns
+                        <p>
+                            Wenn Sie Fragen oder Bedenken bezüglich dieser
+                            Cookie-Richtlinie oder unserer Verwendung von
+                            Cookies haben, kontaktieren Sie uns bitte unter
+                            [Ihre Kontakt-E-Mail].
+                        </p>
+                    </span>
+                </li>
+            </ol>
+        </div>
+    );
 };
 
 export default CookiePolicy;
